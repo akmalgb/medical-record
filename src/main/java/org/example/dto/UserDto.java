e60d51bbd56512e3a57de71039619ccd46e5d18c
akmalgb <akmal.baihaqi@ist.id>
2024-01-01 19:11:38 +0700

    add few columns in users table and UserDto

--- src/main/java/org/example/dto/UserDto.java
+++ src/main/java/org/example/dto/UserDto.java
@@ -1,14 +1,41 @@
 package org.example.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Setter
-@Getter
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
     private Long id;
+    private String nik;
     private String name;
     private String username;
+    private String address;
+    private String dob;
+    private Integer weight;
+    private Integer height;
+    private Boolean isHasInsurance;
+    private String insuranceCarrier;
+    private String policyNumber;
+    private String emergencyContactNumber;
+    private String emergencyContactName;
+    private List<MedicalConditionDto> medicalConditions;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class MedicalConditionDto {
+
+        private Long id;
+        private String doctorName;
+        private String speciality;
+        private String phone;
+        private String notes;
+    }
 
 }
--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Date;
+import java.util.List;
+
 @Data
 @Entity
 @Table(name = "users")
@@ -19,9 +22,42 @@ public class User {
     @Column(name = "id")
     private Long id;
 
+    @Column(length = 16, name = "nik")
+    private String nik;
+
     @Column(name = "name")
     private String name;
 
     @Column(name = "username")
     private String username;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "dob")
+    private Date dob;
+
+    @Column(name = "weight")
+    private Integer weight;
+
+    @Column(name = "height")
+    private Integer height;
+
+    @Column(name = "is_has_insurance")
+    private Boolean isHasInsurance;
+
+    @Column(name = "insurance_carrier")
+    private String insuranceCarrier;
+
+    @Column(name = "policy_number")
+    private String policyNumber;
+
+    @Column(name = "emergency_contact_number")
+    private String emergencyContactNumber;
+
+    @Column(name = "emergency_contact_name")
+    private String emergencyContactName;
+
+    @OneToMany(mappedBy = "user")
+    private List<MedicalCondition> medicalConditions;
 }
