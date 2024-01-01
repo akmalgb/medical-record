341ded4ab3ac0716e850ab49111696b81b24732a
akmalgb <akmal.baihaqi@ist.id>
2024-01-01 19:13:24 +0700

    changes in store user data for save medical conditions

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -2,25 +2,33 @@ package org.example.controller;
 
 import org.example.dto.ResponseDto;
 import org.example.dto.UserDto;
+import org.example.model.MedicalCondition;
 import org.example.model.User;
+import org.example.repository.MedicalConditionRepository;
 import org.example.repository.UserRepository;
+import org.example.util.DateUtil;
 import org.example.util.ResponseUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
+    private final MedicalConditionRepository medicalConditionRepository;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, MedicalConditionRepository medicalConditionRepository) {
         this.userRepository = userRepository;
+        this.medicalConditionRepository = medicalConditionRepository;
     }
 
     @GetMapping("/list")
@@ -53,9 +61,34 @@ public class UserController {
     public ResponseEntity<ResponseDto<User>> createUser(@RequestBody UserDto req) {
         try {
             User user  = new User();
+            user.setNik(req.getNik());
             user.setName(req.getName());
             user.setUsername(req.getUsername());
+            user.setAddress(req.getAddress());
+            user.setDob(DateUtil.parseDate(req.getDob()));
+            user.setWeight(req.getWeight());
+            user.setHeight(req.getHeight());
+            user.setIsHasInsurance(req.getIsHasInsurance());
+            user.setInsuranceCarrier(req.getInsuranceCarrier());
+            user.setPolicyNumber(req.getPolicyNumber());
+            user.setEmergencyContactNumber(req.getEmergencyContactNumber());
+            user.setEmergencyContactName(req.getEmergencyContactName());
             User _user = userRepository.save(user);
+
+            List<MedicalCondition> medicalConditions = req.getMedicalConditions().stream()
+                    .map(medicalConditionDto -> {
+                        MedicalCondition medicalCondition = new MedicalCondition();
+                        medicalCondition.setUser(_user);
+                        medicalCondition.setDoctorName(medicalConditionDto.getDoctorName());
+                        medicalCondition.setSpeciality(medicalConditionDto.getSpeciality());
+                        medicalCondition.setPhone(medicalConditionDto.getPhone());
+                        medicalCondition.setNotes(medicalConditionDto.getNotes());
+                        return medicalCondition;
+                    })
+                    .collect(Collectors.toList());
+
+            medicalConditionRepository.saveAll(medicalConditions);
+
             return ResponseUtil.success(HttpStatus.CREATED.value(), "data created", _user);
         } catch (Exception e) {
             return ResponseUtil.internalServerError();
